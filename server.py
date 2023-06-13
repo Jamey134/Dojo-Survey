11369df23f039238a7816c052d8c767d418a2c43
James Watson <jameywatson134@gmail.com>
2023-06-12 17:04:07 -0700

    update commit

--- server.py
+++ server.py
@@ -8,14 +8,20 @@ app.secret_key = 'TOPSECRET'
 def homePage():
     return render_template('index.html')
 
+@app.route('/results')
+def resultsPage():
+    return render_template('results.html')
 
-@app.route('/results', methods=['POST'])
+
+@app.route('/results/form', methods=['POST'])
 def results():
     print("Got Post Info")
-    # Here we add two properties to session to store the name and email
-    session['username'] = request.form['name']
-    session['useremail'] = request.form['email']
-    return redirect('/show')
+    # Here we add two properties (key_values) to session to store the name and emailß
+    session['userName'] = request.form['name']
+    session['userLocation'] = request.form['location']
+    session['userLanguage'] = request.form['language']
+    session['userComment'] = request.form['comment']
+    return redirect('/results')
 
 
 
